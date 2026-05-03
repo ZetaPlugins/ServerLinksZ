1db6d17d968fe1318a34b865c1b4c5c155b9f09a
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-05-03 11:35:59 +0200

    Add faststats

--- src/main/java/com/zetaplugins/serverlinksz/ServerLinksZ.java
+++ src/main/java/com/zetaplugins/serverlinksz/ServerLinksZ.java
@@ -4,6 +4,9 @@ import com.zetaplugins.serverlinksz.util.CommandManager;
 import com.zetaplugins.serverlinksz.util.EventManager;
 import com.zetaplugins.serverlinksz.util.LanguageManager;
 import com.zetaplugins.serverlinksz.util.LinkManager;
+import dev.faststats.bukkit.BukkitMetrics;
+import dev.faststats.core.ErrorTracker;
+import dev.faststats.core.data.Metric;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.zetaplugins.serverlinksz.util.bStats.CustomCharts;
 import com.zetaplugins.serverlinksz.util.bStats.Metrics;
@@ -11,6 +14,12 @@ import com.zetaplugins.serverlinksz.util.bStats.Metrics;
 import java.io.File;
 
 public final class ServerLinksZ extends JavaPlugin {
+    private static final int BSTATS_PLUGIN_ID = 22795;
+    private static final String FASTSTATS_TOKEN = "e74ea63e83b6c70ddbd45a8d609f1a03";
+    public static final ErrorTracker FASTSTATS_ERROR_TRACKER = ErrorTracker.contextAware();
+
+    private dev.faststats.core.Metrics faststatsMetrics;
+
     private CommandManager commandManager;
     private LanguageManager languageManager;
     private EventManager eventManager;
@@ -32,12 +41,14 @@ public final class ServerLinksZ extends JavaPlugin {
         commandManager.registerCommands();
 
         initializeBStats();
+        initFastStats();
 
         getLogger().info("ServerLinksZ has been enabled!");
     }
 
     @Override
     public void onDisable() {
+        if (faststatsMetrics != null) faststatsMetrics.shutdown();
         getLogger().info("ServerLinksZ has been disabled!");
     }
 
@@ -62,11 +73,22 @@ public final class ServerLinksZ extends JavaPlugin {
     }
 
     private void initializeBStats() {
-        final int pluginId = 22795;
-        Metrics metrics = new Metrics(this, pluginId);
+        Metrics metrics = new Metrics(this, BSTATS_PLUGIN_ID);
 
         metrics.addCustomChart(CustomCharts.getLanguageChart(this));
         metrics.addCustomChart(CustomCharts.getLinksChart(this));
+    }
+
+    private void initFastStats() {
+        this.faststatsMetrics = BukkitMetrics.factory()
+                .token(FASTSTATS_TOKEN)
+                .errorTracker(FASTSTATS_ERROR_TRACKER)
+                .addMetric(Metric.string("language", () -> getConfig().getString("lang")))
+                .create(this);
+
+        this.faststatsMetrics.ready();
+
+        getLogger().info("FastStats metrics initialized.");
     }
 
     public File getPluginFile() {
